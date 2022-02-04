7217d00c9a6861ea8f191a35419d1e2245a5642b
abdou50 <gadourraddadi@gmail.com>
2022-02-04 04:31:02 -0800

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int  main(void)
+	/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+{
+	char n = 'a';
+
+	for ( n = 'a'; n <= 'z'; n++)
+	{
+	putchar (n);
+	}
+	putchar('\n');
+	return 0 ;
+}
