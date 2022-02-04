653da22a091c0e3f7dcf83c16e31ae6c3b42b799
abdou50 <gadourraddadi@gmail.com>
2022-02-04 04:53:07 -0800

    l

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int  main(void)
+{
+        char n = 'a';
+
+        for (n = 'a'; n <= 'z'; n++)
+        {
+        putchar (n);
+        }
+	for (n = 'A'; n <= 'Z'; n++)
+        {
+        putchar (n);
+        }
+        putchar('\n');
+        return (0);
+}
+
