a1f8d9766718de89334a4d424c3d26e3b52de1d1
abdou50 <gadourraddadi@gmail.com>
2022-02-05 09:07:50 -0800

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #define MAXLEN 256
 #include <unistd.h>
-int main (void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	write (1, "and that piece of art is useful"" - Dora Korpar, 2015-10-19\n", 80);
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 82);
+	return (1);
 
 }
