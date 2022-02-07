93fb4d8647896a1e1e63065cb432b5a356fe0dee
abdou50 <gadourraddadi@gmail.com>
2022-02-07 01:34:42 -0800

    4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 82);
+	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 86);
 	return (1);
 
 }
