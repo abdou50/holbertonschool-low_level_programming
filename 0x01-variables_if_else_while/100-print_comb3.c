e22fec3a85bde45450194f895fa1709a403cb6c7
abdou50 <gadourraddadi@gmail.com>
2022-02-07 03:43:16 -0800

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int  main(void)
+{
+	int a;
+	int b;
+	
+        for ( a = '0' ; a != '9' ; a++ )
+	{
+		for ( b = a+1 ; b <= '9' ; b++ ) 
+		{
+			if ( a != b)
+			{
+				putchar(a);
+				putchar(b);
+			
+			if (a + b != 113)
+			{
+				putchar(' ');
+				putchar(',');
+			}
+			}
+		}
+	}
+	putchar ('\n');
+        return (0);
+}
