712e71a8148f5145fb5313e9006c62a13bc13e83
abdou50 <gadourraddadi@gmail.com>
2022-02-07 02:50:36 -0800

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include <unistd.h>
+#include<unistd.h>
+#include<string.h>
 /**
  * main - Entry point
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 86);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
