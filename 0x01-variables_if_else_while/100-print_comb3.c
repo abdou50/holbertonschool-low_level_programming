643132f1ec437dc34714166b214462720515401e
abdou50 <gadourraddadi@gmail.com>
2022-02-07 03:45:33 -0800

    m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,23 +11,23 @@ int  main(void)
 	int a;
 	int b;
 	
-        for ( a = '0' ; a != '9' ; a++ )
+        for (a = '0' ; a != '9' ; a++)
 	{
-		for ( b = a+1 ; b <= '9' ; b++ ) 
+		for (b = a+1 ; b <= '9' ; b++) 
 		{
-			if ( a != b)
+			if (a != b)
 			{
 				putchar(a);
 				putchar(b);
 			
 			if (a + b != 113)
 			{
-				putchar(' ');
 				putchar(',');
+				putchar(' ');
 			}
 			}
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
         return (0);
 }
