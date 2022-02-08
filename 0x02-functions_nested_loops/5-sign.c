9104288af3cea6f1892af60864d2d9b8ebd6952e
abdou50 <gadourraddadi@gmail.com>
2022-02-08 08:32:50 -0800

    l

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - writes the character n to stdout
+ * * @n: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+	int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
