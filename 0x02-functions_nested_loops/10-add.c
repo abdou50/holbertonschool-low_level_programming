17e42bfc37f9d876e71d420f994be5e461c2496e
abdou50 <gadourraddadi@gmail.com>
2022-02-08 11:51:07 -0800

    l

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+/**
+ *add - writes the character c to stdout
+ * @c1 : The character to print
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
+	int add(int c, int c1)
+{
+	int l;
+
+	l = c + c1;
+	return (l);
+}
