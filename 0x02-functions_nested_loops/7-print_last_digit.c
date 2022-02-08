8326ee4f56ec3cd20fbcca45c437e79dc7a0a198
abdou50 <gadourraddadi@gmail.com>
2022-02-08 11:25:46 -0800

    m

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+/**
+ * _islower - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
+        int print_last_digit(int n)
+{
+	int  l;
+	l = n % 10;
+	_putchar(l);
+	return(0);
+}
