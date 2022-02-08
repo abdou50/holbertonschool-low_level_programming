163408796c24d0800f74f9fcec75c0284f6de9ec
abdou50 <gadourraddadi@gmail.com>
2022-02-08 10:56:17 -0800

    l

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *  _abs - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
+int _abs(int n);
+{
+	n = abs(n);
+	_putchar(n);
+return (0);
+}
