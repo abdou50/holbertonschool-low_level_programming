099c327f8005d6c5e37e072d8d7266e8eb0ef67c
abdou50 <gadourraddadi@gmail.com>
2022-02-08 03:19:48 -0800

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,9 +2,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet- writes the character c to stdout
+ * @c: The character to print
  *
- * Return: Always 0.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 void print_alphabet(void);
 {
