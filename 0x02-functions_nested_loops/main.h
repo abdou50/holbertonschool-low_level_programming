0d763bfe7692f77a48bb891adfb1d0e27806b288
abdou50 <gadourraddadi@gmail.com>
2022-02-08 03:18:58 -0800

    l

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+int _putchar(char c)
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+#endif
+
