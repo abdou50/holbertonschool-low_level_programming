d8fc9b87039bbeef9f1ed55e63aa896868bc44e8
abdou50 <gadourraddadi@gmail.com>
2022-02-09 02:18:04 -0800

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - writes the character c to stdout
- * @c: The character to print
+ * print_to_98- writes the character n to stdout
+ * @n: The character to print
  *
  * Return: On success 1
  */
 void print_to_98(int n)
 {
-	int i; 
-        if (n <= 98)
-        {
-		for (i = n ; i <= 98 ; i++)
-			printf("%d ," ,i);
-			if (i = 98)
-				printf("%d",i);
-        }
-        else if (n >= 98)
-        {
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n ; i <= 98; i++)
+		{
+		if (i != 98)
+		{
+		printf("%d ,", i);
+		}
+		else
+		printf("%d", i);
+		}
+	}
+	else if (n >= 98)
+	{
 		for (i = n ; i >= 98; i--)
-			printf("%d ," ,i);
-			if ("%d",
-        }
-	else 
-		printf("%d" ,n);
+		{
+			if (i != 98)
+			{
+			printf("%d ,", i);
+			}
+			else
+			printf("%d", i);
+		}
+	}
+	else
+		printf("%d", n);
 	printf("\n");
 }
