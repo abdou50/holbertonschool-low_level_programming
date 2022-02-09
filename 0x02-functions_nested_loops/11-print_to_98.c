5176c60d2f5ee3b4c2276b4aefac2f6d16532a6a
abdou50 <gadourraddadi@gmail.com>
2022-02-09 01:31:17 -0800

    l

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ */
+void print_to_98(int n)
+{
+	int i; 
+        if (n <= 98)
+        {
+		for (i = n ; i <= 98 ; i++)
+			printf("%d ," ,i);
+			if (i = 98)
+				printf("%d",i);
+        }
+        else if (n >= 98)
+        {
+		for (i = n ; i >= 98; i--)
+			printf("%d ," ,i);
+			if ("%d",
+        }
+	else 
+		printf("%d" ,n);
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
 #endif
 
