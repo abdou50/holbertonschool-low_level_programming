0e43820512387ee9ea0aaa73359adf541cf69aa1
abdou50 <gadourraddadi@gmail.com>
2022-02-10 01:01:42 -0800

    l

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @b: The character to print
+ * @a: The character to print
+ *
+ * Return: On success 1
+ */
+int mul(int a, int b)
+	{
+	return (a * b);
+	}
