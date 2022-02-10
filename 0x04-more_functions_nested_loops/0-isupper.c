b3e509ad37835c688f6023a42ea1bb1efe30d8df
abdou50 <gadourraddadi@gmail.com>
2022-02-10 00:53:31 -0800

    l

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _islower - writes the character c to stdout
+ * _isupper- writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1
  */
-   int _isupper(int c) 
+	int _isupper(int c)
 {
-	if ( c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 		return (1);
-	else 
+	else
 		return (0);
 
 }
