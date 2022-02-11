009b4b2e8750ca4ee7d8aa165817b8f6b8c42312
abdou50 <gadourraddadi@gmail.com>
2022-02-11 10:52:28 -0800

    l

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,8 +30,8 @@ void print_remaining_days(int month, int day, int year)
         }
         else
         {
-            printf("Day of the year: %d\n", day);
+            printf("Day of the year: %d\n", (day - 1));
             printf("Remaining days: %d\n", 365 - day);
         }
     }
-}i
+}
