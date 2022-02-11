385c130b130c56aa6f9197b90374c9a4e741d495
abdou50 <gadourraddadi@gmail.com>
2022-02-11 01:09:17 -0800

    l

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
