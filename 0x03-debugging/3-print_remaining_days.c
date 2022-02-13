92ef0578c1fdff8ce8dd120ef7a18ed8250146fd
abdou50 <gadourraddadi@gmail.com>
2022-02-13 02:07:31 -0800

    l

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,10 @@
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int year
+		)
 {
-    if ((year % 4 == 0 && year % 400 == 0) || (year % 100 != 0))
+    if ((year % 4 == 0 ||  year % 400 == 0) && (year % 100 == 0))
     {
         if (month >= 3 && day >= 60)
         {
@@ -28,10 +29,10 @@ void print_remaining_days(int month, int day, int year)
         {
             printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
         }
-	else
-	{
-    	printf("Day of the year: %d\n", (day - 1));
-        printf("Remaining days: %d\n", 365 - day);
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
         }
     }
 }
