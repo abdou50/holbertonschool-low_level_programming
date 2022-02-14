6eac4fabd6f8a648df7ef2c9420a35175c486bfa
abdou50 <gadourraddadi@gmail.com>
2022-02-14 01:31:33 -0800

    l

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ */
+int _strlen(char *s)
+{
+	int k;
+
+	k = strlen(s);
+	return (k);
+}
