792c850760de76f35f217a82662b7f00b013aa8b
abdou50 <gadourraddadi@gmail.com>
2022-02-14 13:14:24 -0800

    l

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,14 +18,14 @@ for (i = 0 ; str[i] != '\0'; i++)
 }
 if (k % 2 == 1)
 {
-for (i = ((k - 1) / 2) ; i < k - 1 ; i++)
+for (i = ((k - 1) / 2) ; i <= k - 1 ; i++)
 {
 	_putchar(str[i]);
 }
 }
 else
 {
-for (i = ((k) / 2) ; i < k - 1 ; i++)
+	for (i = ((k) / 2) ; i <= k - 1 ; i++)
 {
 	_putchar(str[i]);
 }
