aeff0dda34fd7de04a85949b59f115e8efef4f1c
abdou50 <gadourraddadi@gmail.com>
2022-02-14 04:27:11 -0800

    l

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts2 - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts2(char *str)
+{
+	int k;
+	int i;
+	int j;
+
+	k = 0;
+for (i = 0 ; str[i] != '\0'; i++)
+{
+	k++;
+}
+for (j = 0 ; j <= k; j++)
+{
+	_putchar(str[j]);
+	j++;
+}
+_putchar('\n');
+}
