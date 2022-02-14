0596640f2101f639042e9458cf04b7474f3c84ed
abdou50 <gadourraddadi@gmail.com>
2022-02-14 01:40:33 -0800

    l

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
