78d56d6bcdba834838b790fbccd584b6f51444bf
abdou50 <gadourraddadi@gmail.com>
2022-02-14 02:03:04 -0800

    l

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\n');
+}
