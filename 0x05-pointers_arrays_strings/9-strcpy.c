de75d0f86ffdd3a030e4c76154bc47654e6f4b08
abdou50 <gadourraddadi@gmail.com>
2022-02-14 12:17:32 -0800

    l

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - writes the character c to stdout
+ * @dest: The character to print
+ * @src: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int k, i;
+
+	for (i = 0; src[i] != '\0' ; i++)
+	{
+		k++;
+	}
+	for (i = 0; i <= k ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest = dest + '\0';
+	return (dest);
+
+}
