895071cede2b80aa9dce13070fbc57dac297473d
abdou50 <gadourraddadi@gmail.com>
2022-02-17 00:34:52 -0800

    l

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - writes the character c to stdout
+ * @s1: The character to print
+ * @s2: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int k, i, ls1, ls2;
+
+	ls1 = strlen(s1);
+	ls2 = strlen(s2);
+
+for (i = 0; i < ls1  && i < ls2; i++)
+k = s1[i] - s2[i];
+return (k);
+}
