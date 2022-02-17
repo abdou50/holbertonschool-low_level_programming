28ab43d7b84970d960f6773785cc7de07ca05353
abdou50 <gadourraddadi@gmail.com>
2022-02-17 00:28:59 -0800

    l

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *reverse_array - writes the character c to stdout
+ * @a: The character to print
+ * @n : the character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp = 0;
+	int k;
+
+	if (n % 2 == 0)
+	{
+	while (i != n - 1)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;
+		n--;
+		i++;
+	}
+	}
+	else
+	{
+		k = n / 2;
+		while (k != i && k != n - 2)
+		{
+			tmp = a[i];
+			a[i] = a[n - 1];
+			a[n - 1] = tmp;
+			n--;
+			i++;
+		}
+	}
+}
