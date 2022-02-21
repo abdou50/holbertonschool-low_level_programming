4393c202f3b65498d3b0066863de72b3dc3eb4e0
abdou50 <gadourraddadi@gmail.com>
2022-02-21 03:10:14 -0800

    l

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,13 +1,20 @@
 #include "main.h"
+/**
+ *print_chessboard - writes the character c to stdout
+ * @a: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-for ( i = 0 ; i < 8; i++)
+for (i = 0 ; i < 8; i++)
 {
 		for (j = 0 ; j < 8 ; j++)
 		{
-		putchar( a[i][j]);
+		_putchar(a[i][j]);
 		}
-		putchar('\n');
+		_putchar('\n');
 }
 }
