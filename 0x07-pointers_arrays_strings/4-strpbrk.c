b227ce2a1175172645b4e9ed56f83496815c3b21
abdou50 <gadourraddadi@gmail.com>
2022-02-22 02:06:06 -0800

    l

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * _strpbrk - writes the character c to stdout
+ * @s: The character to print
+ * @accept: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, n, l, k;
+
+	k = strlen(s);
+	l = strlen(accept);
+	for (i = 0; i < k ; i++)
+	{
+		j = 0;
+		while (s[i] != accept[j] && j < l)
+		{
+			j++;
+		}
+	}
+	if (j < l)
+	{
+		for (n = 0; n < j - 2; n++)
+		{
+			s++;
+		}
+		return (s);
+		j = l;
+	}
+	else
+		return (NULL);
+}
