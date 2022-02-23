d196fd0fd4a01156caf8482389ddfc0dfce33aff
abdou50 <gadourraddadi@gmail.com>
2022-02-23 01:35:32 -0800

    l

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - writes the character c to stdout
+ * @s: The character to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
