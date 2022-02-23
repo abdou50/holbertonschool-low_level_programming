07169fb27d7c7b340b60ef4be02e0eb6dd36d50b
abdou50 <gadourraddadi@gmail.com>
2022-02-23 10:44:57 -0800

    l

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,16 +2,16 @@
 /**
  * _pow_recursion - writes the character c to stdout
  * @y: The character to print
- * @x:int 
+ * @x:int
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _pow_recursion(int x, int y)
 {
-	 if (y == 0)
-		return (0);
-	 if (y < 0)
-		 return (-1);
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
 	return (x * _pow_recursion(x, y - 1));
 }
