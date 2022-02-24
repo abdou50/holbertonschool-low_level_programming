4743ac1cdfa3b5d4e9e19066e5fefa72873c99c0
abdou50 <gadourraddadi@gmail.com>
2022-02-24 14:40:40 -0800

    l

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,21 +4,22 @@
  * @s: The character to print
  * @j :length of string
  * @i : compteur
- * 
- *
+ * @l : middle
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int hichem(char *s, int j, int i)
+int hichem(char *s, int j, int l, int i)
 {
+	if (j == 0)
+		return (0);
 	if (s[i] != s[j])
 		return (0);
-	if (s[i] == s[j] && i == j)
+	if (s[i] == s[j] && (i == j || j - 1 == i))
 		return (1);
-	return (hichem(s, j - 1, i + 1));
+	return (hichem(s, j - 1, l, i + 1));
 }
 /**
- * _is_palindome - writes the character c to stdout
+ * is_palindrome - writes the character c to stdout
  * @s: The character to print
  *
  * Return: On success 1.
@@ -26,6 +27,5 @@ int hichem(char *s, int j, int i)
  */
 int is_palindrome(char *s)
 {
-	return (hichem(s, strlen(s) - 1, 0));
+	return (hichem(s, strlen(s) - 1, strlen(s) / 2, 0));
 }
-
