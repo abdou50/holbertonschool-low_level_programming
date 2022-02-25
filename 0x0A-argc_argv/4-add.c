05ec42557d494f58f12fccdf12576f1b2c114ea9
abdou50 <gadourraddadi@gmail.com>
2022-02-25 09:12:59 -0800

    fic

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc < 2)
+	if (argc <= 2)
 	{
 		printf("0\n");
 	}
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 			if (!isdigit(*argv[i]))
 			{
 				printf("Error\n");
-				return (0);
+				return (1);
 			}
 			else
 			{
