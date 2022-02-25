fc112c4f5362f132e4923fa6eed2f1c074a89453
abdou50 <gadourraddadi@gmail.com>
2022-02-25 09:09:31 -0800

    fic

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - writes the character c to stdout
+ * @argc: count arguments
+ * @argv: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (0);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	return (1);
+}
