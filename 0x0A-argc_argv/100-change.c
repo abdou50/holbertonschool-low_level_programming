054ae86d77fcfa0ff810909bdcf1de389d8b801f
abdou50 <gadourraddadi@gmail.com>
2022-02-25 12:58:56 -0800

    l

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,25 +8,25 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int i, l;
-       	int j = 0;	
-	int tab[] = {25, 10, 5, 2, 1};
+int i, l;
+int j = 0;
+int tab[] = {25, 10, 5, 2, 1};
 
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-l = atoi(argv[0]);
+l = atoi(argv[1]);
 if (l < 0)
 {
 printf("0\n");
 }
 for (i = 0; i < 5; i++)
 {
-if (l > tab[i])
+if (l >= tab[i])
 {
 j += (l / tab[i]);
 l = l % tab[i];
@@ -34,8 +34,8 @@ l = l % tab[i];
 if (l == 0)
 {
 printf("%d\n", j);
-break;
+i = 5;
 }
 }
-return (0);
+return (2);
 }
