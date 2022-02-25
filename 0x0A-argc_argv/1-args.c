d14bef794e8ec4501a77b54446273af2bf3e04a7
abdou50 <gadourraddadi@gmail.com>
2022-02-25 04:14:26 -0800

    l

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - writes the character c to stdout
+ * @argc: count arguments
+ * @argv: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc -1);
+return (1); 
+}
