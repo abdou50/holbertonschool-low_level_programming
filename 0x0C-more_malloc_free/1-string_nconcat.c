08e793b0dc15b4dcd6c73687076dee0e7e13bfbd
abdou50 <gadourraddadi@gmail.com>
2022-03-03 11:47:34 -0800

    l

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		p[i] = s1[i];
 	for (k = 0; k < n; k++)
-		p[i + k] = s2[k];
-	p[i + k] = '\0';
+		p[i] = s2[k];
+		i++;
+	p[i] = '\0';
 	return (p);
 }
