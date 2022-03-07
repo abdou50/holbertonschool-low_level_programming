e072eab60dfc96d00e61fbec4fd814f562b61402
abdou50 <gadourraddadi@gmail.com>
2022-03-07 03:43:43 -0800

    l

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - writes the character c to stdout
+ * @void: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+	printf("%s\n",__FILE__);
+	return (0);
+}
