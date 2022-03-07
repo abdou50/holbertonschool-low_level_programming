69eec61aeed64e79ef66022b6700b52e24e2609f
abdou50 <gadourraddadi@gmail.com>
2022-03-07 10:38:22 -0800

    l

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - writes the character c to stdout
+ * @name: The character to print
+ * @age: age of the dog
+ * @owner: owner
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *file;
+	int k, l;
+
+	if (name == NULL || owner == NULL || age < 0)
+		return (NULL);
+	file = malloc(sizeof(dog_t));
+	if (file == NULL)
+		return (NULL);
+	k = strlen(name);
+	(*file).name = malloc(sizeof(char) * (k + 1));
+	if ((*file).name == NULL)
+		return (NULL);
+	l = strlen(owner);
+	(*file).owner = malloc(sizeof(char) * (l + 1));
+	if ((*file).owner == NULL)
+		return (NULL);
+	(*file).name = strcpy((*file).name, name);
+	(*file).age = age;
+	(*file).owner = strcpy((*file).owner, owner);
+	return (file);
+
+}
