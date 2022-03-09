59f46afb1f521496cd550ef575784cc9a67c8fa8
abdou50 <gadourraddadi@gmail.com>
2022-03-09 03:14:06 -0800

    l

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - writes the character c to stdout
+ * @array: The character to print
+ * @size: the size
+ * @cmp: function
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+return (-1);
+}
