a692475d78c327aaa0d6aa3598f9273dc9ce2e21
abdou50 <gadourraddadi@gmail.com>
2022-03-10 05:40:02 -0800

    l

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
 /**
  * print_numbers - print the number in a line
  * @separator: between each number
@@ -12,12 +13,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	int x;
 
+	if (n == 0)
+	{
+		printf("\n");
+		exit (0);
+	}
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
 		if (i < n - 1 && separator != NULL)
-			printf("%d%c ", x, *separator);
+			printf("%d%s ", x, separator);
 		else
 			printf("%d", x);
 	}
