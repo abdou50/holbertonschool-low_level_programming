e72111f44a413ad47bb051cd1cfe99cd7240bf39
abdou50 <gadourraddadi@gmail.com>
2022-03-10 02:29:33 -0800

    l

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - writes the character c to stdout
+ * @separator: The character to print
+ * @n: the size of elements
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *x;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+	x = va_arg(args, char *);
+		if (i < n - 1 && *separator != NULL)
+		{
+			if (x == NULL)
+				printf("(nil)%c ", *separator);
+			else
+				printf("%s%c ", x, *separator);
+		}
+		else
+		{
+			if (x == NULL)
+				printf("(nil)");
+			else
+				printf("%s ", x);
+
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
