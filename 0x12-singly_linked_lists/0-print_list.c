a6119cc8b114cfb619b08544f89ba8d1162ed5b9
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-17 22:27:50 +0200

    l

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ unsigned int  i = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%u] %s\n", 0, "(nil)");
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
