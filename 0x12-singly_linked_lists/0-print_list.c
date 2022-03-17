ac4838bab5ada3dc766aeac7879c25f0b501462b
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-17 14:27:49 +0200

    l

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * print_list - print the element in a specific list
+ * @h: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+size_t print_list(const list_t *h)
+{
+size_t n = 0;
+
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+n++;
+h = h->next;
+}
+return (n);
+}
