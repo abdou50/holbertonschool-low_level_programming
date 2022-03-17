12d2b75d12f2eefc66d2d3d34aa1f37556ebbefd
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-17 17:34:40 +0200

    l

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t n = 0;
 
 while (h != NULL)
 {
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%d] %s\n", h->len, h->str);
-n++;
-h = h->next;
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%d] %s\n", h->len, h->str);
+	n++;
+	h = h->next;
 }
 return (n);
 }
