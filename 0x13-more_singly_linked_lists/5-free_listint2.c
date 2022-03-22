ef9c8d631fdcffe43c7e8393c809dec7a83c4690
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-22 15:20:46 +0200

    listint

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2- writes the character c to stdout
+ * @head: The head
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ops;
+
+while (*head)
+{
+ops = (*head)->next;
+free(*head);
+*head = ops;
+}
+}
