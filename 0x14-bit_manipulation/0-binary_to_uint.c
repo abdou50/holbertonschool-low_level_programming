383031da86e1ab57ff6e407321b04a9eee5eea78
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-24 16:41:46 +0200

    l

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,28 +6,27 @@
  * Return: On success 1.
  * On error, 0 is returned, and errno is set appropriately.
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int k = 1;
-unsigned int result = 0;
-int count;
-unsigned int length;
+	unsigned int k = 1;
+	unsigned int result = 0;
+	int count;
+	unsigned int length;
 
-length = strlen(b);
-if (*b == '\0')
+	length = strlen(b) - 1;
+	if (*b == '\0' && b == NULL)
+		return (0);
+	for (count = length; count >= 0; count--)
+	{
+	if (b[count] != '0' && b[count] != '1')
+	{
 	return (0);
-for (count = length - 1; count >= 0; count--)
-{
-if (b[count] != '0' && b[count] != '1')
-{
-	return (0);
-}
+	}
 	if (b[count] == '1')
 	{
 	result += k;
 	}
 	k *= 2;
 	}
-return (result);
+	return (result);
 }
