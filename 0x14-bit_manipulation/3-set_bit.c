d8a06b76576b457d19ee426e29ed009b6e7dff45
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-24 20:07:47 +0200

    l

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_bit - function to set the bit
+ * @n: integer;
+ * @index:the index;
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	*n |= 1 << index;
+	return (1);
+}
