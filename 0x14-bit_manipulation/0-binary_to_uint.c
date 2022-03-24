4c508b612abb40b147ef782bcddabc1dc7000d23
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-24 14:24:51 +0200

    l

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint- convert from binary to integer
+ * @b: the string to convert
+ *
+ * Return: On success 1.
+ * On error, 0 is returned, and errno is set appropriately.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int k = 1;
+unsigned int result = 0;
+int count;
+unsigned int length;
+
+length = strlen(b);
+for (count = length - 1; count >= 0; count--)
+{
+if (b[count] != '0' && b[count] != '1')
+	return (0);
+	if (b[count] == '1')
+	{
+	result += k;
+	}
+	k *= 2;
+	}
+return (result);
+}
