0665766fd68223c56a4a09bcc36f0c10e516c251
abdou50 <abdelkaderraddadi988@gmail.com>
2022-03-28 15:09:00 +0300

    l

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * read_textfile- is to read and print the text in a specific file
+ *
+ * @filename:  the name of the file
+ * @letters: size of letters
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, k;
+	char *buffer = malloc(sizeof(char *) * letters);
+
+	if (filename == NULL)
+		return (0);
+	if (buffer == NULL)
+		return (0);
+	file = open(filename, O_RDONLY, 0600);
+	if (file == -1)
+		return (0);
+	k = read(file, buffer, letters);
+	write(STDOUT_FILENO, buffer, k);
+	free(buffer);
+	close(file);
+	return (k);
+}
