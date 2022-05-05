5e61801a07985a7fb1da44cb5407de4635ac79b1
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-05 17:33:17 +0300

    l

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index- get node at index.
+ * @head: Pointer to next node.
+ * @index: int
+ * Return: nodes number.
+**/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i != index; i++)
+		head = head->next;
+	return (head);
+}
