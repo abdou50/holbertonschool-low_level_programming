39809f64190bd1990efc1b8f58d6f4b274673d2c
abdou50 <abdelkaderraddadi988@gmail.com>
2022-05-05 17:42:09 +0300

    l

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -8,11 +8,14 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
+
 	if (head == NULL)
 		return (NULL);
 	for (i = 0; i != index; i++)
+	{
+		if (head->next == NULL)
+			return (NULL);
 		head = head->next;
-	if (i > index)
-		return (NULL);
+	}
 	return (head);
 }
